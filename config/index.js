4a9a1fa3cac5415d6f0dd0f25539eb1750048637
Mohen Mondal <37147607+kumohen@users.noreply.github.com>
2020-11-13 00:59:01 +0530

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBWn7-IRJ5tR22Go8tm_1rZzN8aSlsfg8s",
+    apiKey: "8s",
     authDomain: "fir-rn-5de34.firebaseapp.com",
-    databaseURL: "https://fir-rn-5de34.firebaseio.com",
+    databaseURL: "",
     projectId: "fir-rn-5de34",
-    storageBucket: "fir-rn-5de34.appspot.com",
-    messagingSenderId: "483528562278",
-    appId: "1:483528562278:web:423fb56d179b4267f4ffa7",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1:",
     measurementId: "G-JEEJLMXLK4"
   };
